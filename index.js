08956c332ee98d26273cf9b66a5b25b599eb8fab
Fadwabaali <Fadwaaitbaali2@gmail.com>
2024-11-26 11:22:56 +0100

    Navbar and make it responsive

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ const menuToggler = document.querySelector('.menu__toggler');
 menuToggler.addEventListener('click', () => {
     nav.classList.toggle('open__menu');
 });
+
+/*=========== DARK__MODE ===========*/
+
+const darkMode = document.querySelector('.dark__mode'),
+    body = document.querySelector('body');
+
+  darkMode.addEventListener('click', () => {
+    body.classList.toggle('dark');
+  });
