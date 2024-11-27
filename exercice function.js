855d376739778e804b9a834eb4ef5f55518811d4
Amal-2024 <amalrherari@gmail.com>
2024-11-27 15:33:25 +0100

    footer

--- exercice function.js
+++ exercice function.js
@@ -1,6 +1,5 @@
-function(string){
-    return (firstletter)
-};
-function(myfunction)(Number);
-if (Number =%==2)
-    return(Number is pair)
+//Write a function that returns "Hello, World!".
+function saluer ("Hello,Word!") {
+    return(String)
+}
+console.log(saluer);
